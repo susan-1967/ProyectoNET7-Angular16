7d58bc080a21c3bb0dec012854470d90bad3d4d2
susan-1967 <susy15_j@hotmail.com>
2023-09-25 21:31:31 -0500

    Final

---
+++ VintageStore/insertprendas.sql
@@ -0,0 +1,45 @@
+USE [VintageDb]
+
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chompa', N'Chompa', CAST(6.00 AS Decimal(11, 2)), 1, CAST(N'2023-09-15T12:00:00.000' AS DateTime), N'C:\Susan\Proyecto\VintageStore\imagenChompa.png', 5, 0, CAST(N'2023-09-23T09:17:36.3151858' AS DateTime2), CAST(N'2023-09-23T23:17:56.2891138' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'vestido', N'vestido de primera', CAST(40.00 AS Decimal(11, 2)), 3, CAST(N'2023-09-16T20:00:00.000' AS DateTime), N'http://localhost:5035/imagen/vestido.jpg', 4, 0, CAST(N'2023-09-23T20:40:05.7351924' AS DateTime2), CAST(N'2023-09-23T23:18:28.9364337' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chompa', N'Chompa de lana muy linda', CAST(56.00 AS Decimal(11, 2)), 3, CAST(N'2023-09-04T20:00:00.000' AS DateTime), N'http://localhost:5035/imagen/Vestido.png', 4, 0, CAST(N'2023-09-23T20:51:26.0755270' AS DateTime2), CAST(N'2023-09-23T23:18:01.8405659' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chompas', N'Chompas de lima', CAST(30.00 AS Decimal(11, 2)), 2, CAST(N'2023-09-18T20:00:00.000' AS DateTime), N'http://localhost/imagen/demo.png', 4, 0, CAST(N'2023-09-23T21:06:39.8209595' AS DateTime2), CAST(N'2023-09-23T23:18:34.4844294' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'vestido_mas', N'uno mas uno', CAST(7.00 AS Decimal(11, 2)), 1, CAST(N'2023-04-12T20:00:00.000' AS DateTime), N'http://localhost/images/luis.jpg', 9, 0, CAST(N'2023-09-23T21:12:49.7232659' AS DateTime2), CAST(N'2023-09-23T23:18:44.2634537' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'vestido_de_meo', N'misma_mesa', CAST(99.00 AS Decimal(11, 2)), 1, CAST(N'2023-09-12T20:00:00.000' AS DateTime), N'C:\Susan\Proyecto\VintageStore\imagen\string.png', 89, 0, CAST(N'2023-09-23T21:58:29.2561886' AS DateTime2), CAST(N'2023-09-23T23:18:39.5095148' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'ventas', N'ventas la minudeo', CAST(2.00 AS Decimal(11, 2)), 1, CAST(N'2023-09-30T12:00:00.000' AS DateTime), N'C://Susan//Proyecto//VintageStore//imagen//julio.png', 2, 0, CAST(N'2023-09-23T22:10:52.4020512' AS DateTime2), CAST(N'2023-09-23T23:18:07.7624187' AS DateTime2), 0)
+GO
+INSERT [dbo].[Producto] ([Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Blazer vintage blanco y negro', N'Chaqueta corta en mezcla de lana con estampado de pata de gallo, con detalles de cuello en contraste y cierre de un botón.', CAST(100.00 AS Decimal(11, 2)), 4, CAST(N'2023-08-13T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda3.jpeg', 3, 0, CAST(N'2023-09-23T23:12:17.5442327' AS DateTime2), CAST(N'2023-09-24T21:52:10.4214591' AS DateTime2), 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Vestido de Mujer Blanco Xhilaration', N'Material: Algodón
+Tamaño: S
+Condición: Usado
+Color: Blanco
+Género: Femenino
+Edades: Adultos
+Mangas: Largas
+Cuello: Redondo', CAST(45.00 AS Decimal(11, 2)), 1, CAST(N'2023-09-22T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda1.jpeg', 2, 0, CAST(N'2023-09-24T06:57:29.5187172' AS DateTime2), CAST(N'2023-09-24T21:37:09.2107739' AS DateTime2), 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Vestido Cheongsam Qipao', N'Mandarín de Jacquard para mujer, vestidos tradicionales chinos de manga corta, vestido de fiesta Formal de seda Sexy', CAST(462.00 AS Decimal(11, 2)), 2, CAST(N'2023-09-22T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda2.jpeg', 30, 0, CAST(N'2023-09-24T10:49:22.6287672' AS DateTime2), CAST(N'2023-09-24T21:49:35.1378056' AS DateTime2), 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Vestido marron', N'Diseño personalizado los cierres rápidos funcional a las niñas de bolsillos de puente de gamuza color marrón de la moda vestidos de Fitness', CAST(200.00 AS Decimal(11, 2)), 3, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda4.jpeg', 30, 0, CAST(N'2023-09-24T22:00:50.5358496' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chaqueta de Camionero', N'Chaqueta de Camionero De Colección Levis Denim Cuero Gamuza Patchwork Hecha en EE. UU. Hombres', CAST(100.00 AS Decimal(11, 2)), 4, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda5.jpeg', 34, 0, CAST(N'2023-09-24T22:05:11.2091944' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chaqueta blanca - con bordes', N'Este blazer es genial como chaqueta de primavera / verano / otoño y se ve bastante elegante con su blanco y las aplicaciones coloridas y doradas y perlas, pero al mismo tiempo hace un look casual.', CAST(60.00 AS Decimal(11, 2)), 2, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda6.jpeg', 600, 0, CAST(N'2023-09-24T22:08:36.3583206' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chaqueta de algodón multicolor', N'Chaqueta de algodón multicolor hecha a mano, diseño étnico impreso, estilo vaquero Vintage, Kantha, venta al por mayor', CAST(70.00 AS Decimal(11, 2)), 2, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda7.jpeg', 10, 0, CAST(N'2023-09-24T22:10:07.2350653' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Cárdigan Vintage', N'Cárdigan Vintage para mujer, suéteres de punto con capucha, estilo Retro de pasarela, holgado, con gancho y flores, moda de invierno', CAST(30.00 AS Decimal(11, 2)), 2, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda8.jpeg', 300, 0, CAST(N'2023-09-24T22:12:10.1758454' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'Chaqueta de cuero', N'Chaqueta de cuero de gamuza marrón para mujer, abrigo con flecos de estilo occidental', CAST(20.00 AS Decimal(11, 2)), 3, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda9.jpeg', 40, 0, CAST(N'2023-09-24T22:14:22.4106570' AS DateTime2), NULL, 1)
+GO
+INSERT [dbo].[Producto] ( [Nombre], [Description], [UnitPrice], [TipoId], [DateEvent], [ImageUrl], [Cantidad], [Finalized], [CreationDate], [ModifiedDate], [Status]) VALUES ( N'vestidos vaqueros superventas del low cost', N'Los vestidos vaqueros tendencia más ponibles que solucionará tu look diario con mucho estilo', CAST(50.00 AS Decimal(11, 2)), 3, CAST(N'2023-09-24T12:00:00.000' AS DateTime), N'http://localhost:4200/assets/images/prenda11.jpeg', 30, 0, CAST(N'2023-09-24T22:16:56.0661631' AS DateTime2), NULL, 1)
+GO
+SET IDENTITY_INSERT [dbo].[Producto] OFF
+GO
